edb01d5d6116bf837a03ef1968bc34c4b5f61562
Miika Hänninen <miika.hanninen@relexsolutions.com>
2023-06-14 23:15:13 +0300

    Add linting setup

---
+++ lint-staged.config.cjs
@@ -0,0 +1,15 @@
+const typecheck = (_changedTsFiles) => 'npm run check:types'
+const prettier = 'prettier --write --ignore-path .gitignore'
+const eslint =
+  'eslint --ignore-path .gitignore --cache --cache-strategy content --max-warnings=0 --fix'
+
+const renovateConfigFile = 'renovate.json5'
+const renovate = (_changedFiles) =>
+  `docker run --rm -v ".:/usr/src/app" renovate/renovate:slim renovate-config-validator ${renovateConfigFile}`
+
+module.exports = {
+  '*.{ts,mts,cts}': [typecheck, eslint, prettier],
+  '*.{js,mjs,cjs}': [eslint, prettier],
+  '*.{json,json5,yaml,yml,md}': [prettier],
+  [renovateConfigFile]: [renovate],
+}
